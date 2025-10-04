3c5b8f6832d59b9799c199519156de012ec17bb1
saulmanz <saulmanzanares18@gmail.com>
2025-10-03 21:33:26 -0400

    a2 CSS and Bootstrap

--- app/(Kambaz)/Courses/[cid]/Home/status.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/status.tsx
@@ -1,6 +1,6 @@
-import { MdDoNotDisturbAlt } from "react-icons/md";
-import { FaCheckCircle } from "react-icons/fa";
-import { BiImport } from "react-icons/bi";
+import { BsExclamationCircle } from "react-icons/bs";
+import { BsCheckCircle } from "react-icons/bs";
+import { BsDownload } from "react-icons/bs";
 import { LiaFileImportSolid } from "react-icons/lia";
 import { Button } from "react-bootstrap";
 import { BsHouseDoorFill } from "react-icons/bs";
@@ -15,14 +15,14 @@ export default function CourseStatus() {
      <div className="d-flex">
        <div className="w-50 pe-1">
          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
-           <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </Button> </div>
+           <BsExclamationCircle className="me-2 fs-5" /> Unpublish </Button> </div>
        <div className="w-50">
          <Button variant="success" size="lg" className="w-100">
-           <FaCheckCircle className="me-2 fs-5" /> Publish </Button> </div>
+           <BsCheckCircle  className="me-2 fs-5" /> Publish </Button> </div>
      </div>
      <br />
      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
-       <BiImport className="me-2 fs-5" /> Import Existing Content </Button>
+       <BsDownload className="me-2 fs-5" /> Import Existing Content </Button>
      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </Button>
 
