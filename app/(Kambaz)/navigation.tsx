c487046c1678018123fbfef6d28b8b54049cfe85
saulmanz <saulmanzanares18@gmail.com>
2025-10-03 21:40:29 -0400

    a2 CSS and Bootstrap

--- app/(Kambaz)/navigation.tsx
+++ app/(Kambaz)/navigation.tsx
@@ -1,7 +1,8 @@
-import { AiOutlineDashboard } from "react-icons/ai";
-import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
-import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { BsFillJournalBookmarkFill, BsSpeedometer } from "react-icons/bs";
+import { BsPersonCircle } from "react-icons/bs";
+import { BsCalendar2Week } from "react-icons/bs";
+import { BsFillGearFill } from "react-icons/bs";
+import { BsFillInboxesFill } from "react-icons/bs";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
 export default function KambazNavigation() {
@@ -14,42 +15,42 @@ export default function KambazNavigation() {
      </ListGroupItem>
      <ListGroupItem className="border-0 bg-black text-center">
        <Link href="/Account" id="wd-account-link" className="text-white text-decoration-none">
-         <FaRegCircleUser className="fs-1 text-white" />
+         <BsPersonCircle className="fs-1 text-white" />
          <br />
          Account
        </Link>
      </ListGroupItem>
      <ListGroupItem className="border-0 bg-white text-center">
        <Link href="/Dashboard" id="wd-dashboard-link" className="text-danger text-decoration-none">
-         <AiOutlineDashboard className="fs-1 text-danger" />
+         <BsSpeedometer className="fs-1 text-danger" />
          <br />
          Dashboard
        </Link>
      </ListGroupItem>
      <ListGroupItem className="border-0 bg-black text-center">
        <Link href="/Courses" id="wd-courses-link" className="text-white text-decoration-none">
-         <LiaBookSolid className="fs-1 text-white" />
+         <BsFillJournalBookmarkFill className="fs-1 text-white" />
          <br />
          Courses
        </Link>
      </ListGroupItem>
       <ListGroupItem className="border-0 bg-black text-center">
        <Link href="/Calendar" id="wd-calendar-link" className="text-white text-decoration-none">
-         <IoCalendarOutline className="fs-1 text-white" />
+         <BsCalendar2Week className="fs-1 text-white" />
          <br />
          Calendar
        </Link>
      </ListGroupItem>
       <ListGroupItem className="border-0 bg-black text-center">
        <Link href="/Inbox" id="wd-inbox-link" className="text-white text-decoration-none">
-         <FaInbox className="fs-1 text-white" />
+         <BsFillInboxesFill className="fs-1 text-white" />
          <br />
          Inbox
        </Link>
      </ListGroupItem>
       <ListGroupItem className="border-0 bg-black text-center">
        <Link href="/Labs" id="wd-labs-link" className="text-white text-decoration-none">
-         <LiaCogSolid className="fs-1 text-white" />
+         <BsFillGearFill className="fs-1 text-white" />
          <br />
          Labs
        </Link>
