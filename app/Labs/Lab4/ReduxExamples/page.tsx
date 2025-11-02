6c5ea4de311e9cc52ceb6ca8756c132e668a9307
Saul M <saulmanzanares18@gmail.com>
2025-11-01 21:14:36 -0400

    lint fixes

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,11 +1,14 @@
-"use client"
+"use client";
+import { Provider } from "react-redux";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./HelloRedux";
 import TodoList from "./todos/TodoList";
+import store from "../store";
 
 export default function ReduxExamples() {
   return(
+        <Provider store={store}>
     <div>
       <h2>Redux Examples</h2>
       <HelloRedux/>
@@ -13,5 +16,6 @@ export default function ReduxExamples() {
       <AddRedux/>
       <TodoList/>
     </div>
+        </Provider>
   );
 };
